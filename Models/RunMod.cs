722090ca5c70dede55f0a15d13e259093143d5eb
Griffin Puc <griffinpuc@outlook.com>
2019-10-23 17:10:46 -0400

    new model

---
+++ Models/RunMod.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tangen-dataportal-version2.Models
+{
+    public class RunMod
+    {
+
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
+        public int ID { get; set; }
+        public string sampleID { get; set; }
+        public string uniqueID { get; set; }
+        public string downloadDateTime { get; set; }
+        public string assayID { get; set; }
+        public string kitLotID { get; set; }
+        public string instrumentUUID { get; set; }
+        public string instrumentName { get; set; }
+
+        [JsonIgnore]
+        public string rawAddress { get; set; }
+
+        [JsonIgnore]
+        //public ICollection<modelTag> tags { get; set; }
+
+        //public ICollection<modelTarget> targets { get; set; }
+        //public ICollection<modelWell> wells { get; set; }
+
+    }
+}
